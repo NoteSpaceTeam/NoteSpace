bac8ee504ec4ef5fec17c97f5be99d7f38eb3c4a
Guilherme_F <guilherme.ferreira120703@gmail.com>
2024-04-05 23:54:17 +0100

    Started implementing custom service worker

---
+++ code/client/src/pwa/pwa-config.ts
@@ -0,0 +1,20 @@
+import { injectConfig } from './inject-config';
+import { manifestConfig } from './manifest-config';
+import { VitePWAOptions } from 'vite-plugin-pwa';
+
+export const pwaConfig : Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  strategies: 'injectManifest',
+  srcDir: './src/pwa',
+  filename: 'sw.ts',
+  //injectRegister: false,
+  injectManifest:injectConfig,
+  registerType: 'autoUpdate',
+  manifest: manifestConfig,
+  devOptions: {
+    enabled: true,
+    navigateFallback: '../../index.html',
+    type:'module',
+  },
+}
