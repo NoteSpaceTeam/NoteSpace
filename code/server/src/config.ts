0b5342e900aa8d1719f58460b19fb75634a8955d
Ricardo Costa <rcosta.ms358@gmail.com>
2024-06-24 23:01:17 +0100

    Update config.ts

--- code/server/src/config.ts
+++ code/server/src/config.ts
@@ -5,16 +5,15 @@ config();
 
 const SERVER_PORT = parseInt(process.env.PORT || '8080');
 const CLIENT_PORT = parseInt(process.env.CLIENT_PORT || '5173');
-const HOST_IP = process.env.HOST_IP;
-const ORIGIN = [`http://localhost:${CLIENT_PORT}`, 'http://localhost:8080'];
-const SERVER_IP = HOST_IP || 'localhost';
-
-if (HOST_IP) ORIGIN.push(`http://${HOST_IP}:${CLIENT_PORT}`, `http://${HOST_IP}:8080`);
+const SERVER_IP = 'localhost';
+const ORIGIN = '*';
 
 const SERVER_OPTIONS = {
   cors: {
     origin: ORIGIN,
     credentials: true, // allow credentials (cookies, authorization headers, etc.)
+    allowedMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
   },
   connectionStateRecovery: {},
 };
