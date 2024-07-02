e91ec586f386026ea0a1edc981f68129520fed9f
Guilherme_F <guilherme.ferreira120703@gmail.com>
2024-07-02 19:22:33 +0100

    Updates on PWA
    * Dev build working, must check prod

--- code/client/src/pwa/manifest-config.ts
+++ code/client/src/pwa/manifest-config.ts
@@ -1,22 +1,36 @@
 import { ManifestOptions } from 'vite-plugin-pwa';
 
-const manifestConfig: Partial<ManifestOptions> = {
+export const manifestConfig: Partial<ManifestOptions> = {
   name: 'NoteSpace',
   short_name: 'NoteSpace',
-  description: 'A multiplatform note management and sharing app',
+  description: 'A multi-platform real-time note-taking app',
+  start_url: '/',
+  scope: '.',
+  display: 'standalone',
+  background_color: '#ffffff',
   theme_color: '#ffffff',
+  orientation: 'portrait-primary',
   icons: [
     {
-      src: '/assets/icon.png',
+      src: 'pwa-64x64.png',
+      sizes: '64x64',
+      type: 'image/png',
+    },
+    {
+      src: 'pwa-192x192.png',
       sizes: '192x192',
       type: 'image/png',
     },
     {
-      src: '/assets/icon.png',
+      src: 'pwa-512x512.png',
       sizes: '512x512',
       type: 'image/png',
     },
+    {
+      src: 'maskable-icon-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
   ],
 };
-
-export default manifestConfig;
